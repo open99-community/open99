437a402b642aaed5ec1c3d1b883fccebe3a9f036
Pablo G <pablobooks67@gmail.com>
2021-11-09 19:43:50 -0800

    Create sys41.js

---
+++ system/sys41.js
@@ -0,0 +1,37 @@
+var sys41 = {
+  'user': {},
+  'system': {}
+}
+sys41.system.version = '0.1'
+
+//Remove right-click functionality
+document.addEventListener("contextmenu", function(evt) {
+  evt.preventDefault();
+});
+
+//Browser detector
+var nav = navigator.userAgent;
+sys41.user.navigatorFull = navigator.userAgent;
+if (nav.indexOf("Firefox") > -1) {
+  sys41.user.navigator = "Mozilla Firefox";
+} else if (nav.indexOf("SamsungBrowser") > -1) {
+  sys41.user.navigator = "Samsung Internet";
+} else if (nav.indexOf("Opera") > -1 || nav.indexOf("OPR") > -1) {
+  sys41.user.navigator = "Opera";
+} else if (nav.indexOf("Trident") > -1) {
+  sys41.user.navigator = "Microsoft Internet Explorer";
+} else if (nav.indexOf("Edge") > -1) {
+  sys41.user.navigator = "Microsoft Edge (Legacy)";
+} else if (nav.indexOf("Edg") > -1) {
+  sys41.user.navigator = "Microsoft Edge";
+} else if (nav.indexOf("Chrome") > -1) {
+  sys41.user.navigator = "Chrome/Chromium";
+} else if (nav.indexOf("Safari") > -1) {
+  sys41.user.navigator = "Apple Safari";
+} else {
+  sys41.user.navigator = "unknown";
+}
+
+for(var i = 0; i < navigator.plugins.length; i++) {
+  //coming soon
+}
