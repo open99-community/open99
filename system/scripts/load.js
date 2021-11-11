50c3c72d86eb18fff03517b37286276033779b2c
Pablo G <pablobooks67@gmail.com>
2021-11-11 15:57:23 -0800

    Update load.js

--- system/scripts/load.js
+++ system/scripts/load.js
@@ -35,3 +35,5 @@ if (nav.indexOf("Firefox") > -1) {
 for(var i = 0; i < navigator.plugins.length; i++) {
   //coming soon
 }
+
+if (location.pathname === / || location.pathname === /index.html) {getElementsByTagName('p')[0].innerHTML = `yay it worked`}
