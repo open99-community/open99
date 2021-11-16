1b03934b909ab6de00e016e24d6779fd6f337189
Pablo G <itspablo@null.net>
2021-11-16 14:04:54 -0800

    sys41 boot api

--- system/scripts/boot.js
+++ system/scripts/boot.js
@@ -1,40 +1,40 @@
 //BOOTSCREEN
+var sys41.boot.html[welcome] = document.getElementById("welcome");
+var sys41.boot.html[dateUserAgent] = document.getElementById("dateUserAgent");
+var sys41.boot.html[devmodeText] = document.getElementById("devmode");
+var sys41.boot.html[server] = document.getElementById("server");
+var sys41.boot.html[port] = document.getElementById("port");
+var sys41.boot.html[plugins] = document.getElementById("plugins");
+var sys41.boot.html[protocol] = document.getElementById("protocol");
+var sys41.boot.html[hash] = document.getElementById("hash");
+
+//Actual boot
 if ($(".bootscreen")[0]){
-    //Variable setup
-    var welcome = document.getElementById("welcome");
-    var dateUserAgent = document.getElementById("dateUserAgent");
-    var devmodeText = document.getElementById("devmode");
-    var server = document.getElementById("server");
-    var port = document.getElementById("port");
-    var plugins = document.getElementById("plugins");
-    var protocol = document.getElementById("protocol");
-    var hash = document.getElementById("hash");
-    //Actual boot
-    welcome.innerHTML = `
+    sys41.boot.html.welcome.innerHTML = `
     system41 presents <a class="rainbow-text">Windows 99</a> version 0.4 (official.dev)<br />
     licensed under the <b>Mozilla Public License</b>` 
-    dateUserAgent.innerHTML = `booting on ` + Date() + `<br />in ` + sys41.user.navigator + `<br />`
+    sys41.boot.html.dateUserAgent.innerHTML = `booting on ` + Date() + `<br />in ` + sys41.user.navigator + `<br />`
     if (!location.port || !location.href === 'https://windows99.vercel.app') {
-        devmodeText.classList.add("hidden")
+        sys41.boot.html.devmodeText.classList.add("hidden")
     } else {
-        devmodeText.innerHTML = `<span class="warning-boot">WARNING: you are running this on a custom or non-production server.<br />
+        sys41.boot.html.devmodeText.innerHTML = `<span class="warning-boot">WARNING: you are running this on a custom or non-production server.<br />
         please be aware this is not an official release of windows99. <a href="https://itspablo.gitbook.io/windows99/forking/faq#unverified_boot_error">learn how to remove this</a></span>`
     };
-    server.innerText = 'Server: ' + location.hostname;
+    sys41.boot.href.server.innerText = 'Server: ' + location.href;
     if (location.port === undefined || location.port === null) {
-      port.classList.add("hidden");
+      sys41.boot.html.port.classList.add("hidden");
     } else {
-      port.innerHTML = 'Port number: ' + location.port
+      sys41.boot.html.port.innerHTML = 'Port number: ' + location.port
     };
     
     for(var i = 0; i < navigator.plugins.length; i++) {
-        plugins.innerHTML = `<p>` + navigator.plugins.name[i] + `</p>`
+        sys41.boot.html.plugins.innerHTML = `<p>` + navigator.plugins.name[i] + `</p>`
     }
     
-    if (protocol === "https:") {
-      protocol.innerHTML = 'Protocol: secure'
+    if (location.protocol === "https:") {
+      sys41.boot.html.protocol.innerHTML = 'Protocol: secure'
     } else {
-      protocol.innerHTML = 'Protocol: ' + location.protocol
+      sys41.boot.html.protocol.innerHTML = 'Protocol: ' + location.protocol
     };
 };
 
