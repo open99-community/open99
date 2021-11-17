9c7eb57e4badc0df4e2f8c538272cae684ed0d8e
Pablo G <itspablo@null.net>
2021-11-16 16:12:17 -0800

    Delete pwa-main.js

--- system/scripts/pwa-main.js
+++
@@ -1,1 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/sw.js").then(function(e){console.log("ServiceWorker registration successful with scope: ",e.scope)},function(e){console.log("ServiceWorker registration failed: ",e)})})
