514d933261f88c3bb33a4f7a07edc146466c211e
Pablo G <itspablo@null.net>
2021-11-21 17:23:37 -0800

    parenthesis
    also im on thanksgiving vac so i may be slow

--- system/scripts/boot.js
+++ system/scripts/boot.js
@@ -14,14 +14,14 @@ if ($(".bootscreen")[0]){
     system41 presents <a class="rainbow-text">Windows 99</a> version 0.4 (official.dev)<br />
     licensed under the <b>Mozilla Public License</b>` 
     sys41.boot.html.dateUserAgent.innerHTML = `booting on ` + Date() + `<br />in ` + sys41.user.navigator + `<br />`
-    if (!location.port || !location.href === 'https://windows99.vercel.app') {
+    if ((!location.port) || (!location.href === 'https://windows99.vercel.app')) {
         sys41.boot.html.devmodeText.classList.add("hidden")
     } else {
         sys41.boot.html.devmodeText.innerHTML = `<span class="warning-boot">WARNING: you are running this on a custom or non-production server.<br />
-        please be aware this is not an official release of windows99. <a href="https://itspablo.gitbook.io/windows99/forking/faq#unverified_boot_error">learn how to remove this</a></span>`
+        please be aware this is not an official release of windows99. <a href="https://itspablo.gitbook.io/windows99/forking/faq#unverified_boot_error"><b>learn how to remove this</b></a></span>`
     };
     sys41.boot.href.server.innerText = 'Server: ' + location.href;
-    if (location.port === undefined || location.port === null) {
+    if ((location.port === undefined) || (location.port === null)) {
       sys41.boot.html.port.classList.add("hidden");
     } else {
       sys41.boot.html.port.innerHTML = 'Port number: ' + location.port
