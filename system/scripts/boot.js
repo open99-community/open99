ebc33a687330eac196a5495afb22d8d57c5cffb1
Pablo <itspablo@null.net>
2021-12-07 11:04:35 -0800

    OH MY GOD BOOT FIX
    #25 should be fixed omg

--- system/scripts/boot.js
+++ system/scripts/boot.js
@@ -2,34 +2,34 @@
 document.onclick = function(e) {e.preventDefault}
 
 //Actual boot
-/*if ($(".bootscreen")[0]){*/
-    sys41.boot.html.welcome.innerHTML = `
+if ($(".bootscreen")[0]){
+    sys41.system.boot.html.welcome.innerHTML = `
     system41 presents <a class="rainbow-text">Windows 99</a> version 0.4 (official.dev)<br />
     licensed under the <b>Mozilla Public License</b>` 
-    sys41.boot.html.dateUserAgent.innerHTML = `booting on ` + Date() + `<br />in ` + sys41.user.navigator + `<br />`
+    sys41.system.boot.html.dateUserAgent.innerHTML = `booting on ` + Date() + `<br />in ` + sys41.user.navigator + `<br />`
     if ((!location.port) || (!location.href === 'https://windows99.vercel.app')) {
-        sys41.boot.html.devmodeText.classList.add("hidden")
+        sys41.system.boot.html.devmodeText.classList.add("hidden")
     } else {
-        sys41.boot.html.devmodeText.innerHTML = `<span class="warning-boot">WARNING: you are running this on a custom or non-production server.<br />
+        sys41.system.boot.html.devmodeText.innerHTML = `<span class="warning-boot">WARNING: you are running this on a custom or non-production server.<br />
         please be aware this is not an official release of windows99. <a href="https://itspablo.gitbook.io/windows99/forking/faq#unverified_boot_error"><b>learn how to remove this</b></a></span>`
     };
-    sys41.boot.href.server.innerText = 'Server: ' + location.href;
+    sys41.system.boot.href.server.innerText = 'Server: ' + location.href;
     if ((location.port === undefined) || (location.port === null)) {
-      sys41.boot.html.port.classList.add("hidden");
+      sys41.system.boot.html.port.classList.add("hidden");
     } else {
-      sys41.boot.html.port.innerHTML = 'Port number: ' + location.port
+      sys41.system.boot.html.port.innerHTML = 'Port number: ' + location.port
     };
     
     for(var i = 0; i < navigator.plugins.length; i++) {
-        sys41.boot.html.plugins.innerHTML = `<p>` + navigator.plugins.name[i] + `</p>`
+        sys41.system.boot.html.plugins.innerHTML = `<p>` + navigator.plugins.name[i] + `</p>`
     }
     
     if (location.protocol === "https:") {
-      sys41.boot.html.protocol.innerHTML = 'Protocol: secure'
+      sys41.system.boot.html.protocol.innerHTML = 'Protocol: secure'
     } else {
-      sys41.boot.html.protocol.innerHTML = 'Protocol: ' + location.protocol
+      sys41.system.boot.html.protocol.innerHTML = 'Protocol: ' + location.protocol
     };
-/*};*/
+};
 
 alert("debug")
 //SET THEME
