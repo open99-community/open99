bb9e704b3073c0bf09a4340d1bfc862bee9ed2fe
Pablo <pablobooks67@gmail.com>
2022-01-20 12:00:16 -0800

    supabase change

--- system/scripts/signin.js
+++ system/scripts/signin.js
@@ -1,14 +1,3 @@
-//windows99 uses firebase for 'local' authentication.
+//windows99 uses supabase for 'local' authentication.
 //when the user is offline, they can choose to sign in anonymously
 //temporarily, anonymous signin is required
-firebase
-  .auth()
-  .signInAnonymously()
-  .then(() => {
-    // Signed in
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert(error.code + " " + error.message);
-  });
