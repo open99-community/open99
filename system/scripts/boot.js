733e3443a265ab13b33df3b3c70eafd4d8d0ef5d
Pablo <pablobooks67@gmail.com>
2022-01-20 12:15:47 -0800

    test

--- system/scripts/boot.js
+++ system/scripts/boot.js
@@ -10,13 +10,15 @@ document.onclick = function (e) {
   sys41.system.boot.add(`
     system41 presents <a class="rainbow-text">Windows 99</a> version 0.4 (official.dev)<br />
     licensed under the <b>Mozilla Public License</b>`)
+  var g = setInterval(getDate, 500);
+  function getDate() {return Date()}
   var dateNav = sys41.system.boot.add(
-    `booting on ` + Date() + `<br />in ` + sys41.user.navigator + `<br />`)
+    `booting on ` + g + `<br />in ` + sys41.user.navigator + `<br />`)
   if (location.href === "https://windows99.vercel.app") {
     dateNav.classList.add("hidden");
   } else {
     sys41.system.boot.add(`<span class="warning-boot">WARNING: you are running this on a custom or non-production server.<br />
-        please be aware this is not an official release of windows99. <a onclick='open("https://itspablo.gitbook.io/windows99/forking/faq#unverified_boot_error", "_blank")'><b>learn how to remove this</b></a></span>`, true)
+        please be aware this is not an official release of windows99. <a href='https://itspablo.gitbook.io/windows99/forking/faq#unverified_boot_error' target="_blank"><b>learn how to remove this</b></a></span>`, true)
   }
   //server
   sys41.system.boot.add("Server: " + location.href)
