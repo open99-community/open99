c21da5971589dc18af1f1df986b0551ec430e449
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-03-24 19:14:37 -0700

    netlify forms

--- src/bugs/bug.js
+++ src/bugs/bug.js
@@ -1,69 +1,16 @@
-//COMING SOON
-document.getElementById("specificApp").addEventListener('click', whereOccurSpecificApp())
+(function () {
+  document.forms[0].addEventListener("submit", handleSubmit);
 
-function whereOccurSpecificApp() {
-  if (document.getElementById("specificApp").checked) {
-    document.getElementById("whichApp").classList.remove("hidden");
-  } else {
-    document.getElementById("whichApp").classList.add("hidden");
-  }
-}
-
-//WEB3FORMS INJECTION
-(function() {
-  let form = document.getElementsByTagName("form")[0];
-  let accessKey = document.createElement("input");
-  accessKey.setAttribute("type", "hidden");
-  accessKey.setAttribute("name", "apikey");
-  accessKey.setAttribute("value", "896d2f9a-f729-443b-be74-041986e1e117"); //Change if this is your own fork
-  let subject = document.createElement("input");
-  subject.setAttribute("type", "hidden");
-  subject.setAttribute("name", "subject");
-  subject.setAttribute("value", "New bug report - Windows99");
-  let version = document.createElement("input");
-  subject.setAttribute("type", "hidden");
-  subject.setAttribute("name", "version");
-  subject.setAttribute(
-    "value",
-    sys41.system.version +
-      " on " +
-      sys41.user.navigatorFull +
-      " using " +
-      sys41.system.channel
-  );
-  let fromName = document.createElement("input");
-  subject.setAttribute("type", "hidden");
-  subject.setAttribute("name", "from_name");
-  subject.setAttribute("value", "Windows99 Bugs");
-  //SUBMISSION SETUP
-  form.addEventListener("submit", function (e) {
-    const formData = new FormData(form);
+  const handleSubmit = (e) => {
     e.preventDefault();
-    let object = {};
-    formData.forEach((value, key) => {
-      object[key] = value;
-    });
-    let json = JSON.stringify(object);
-
-    fetch("https://api.web3forms.com/submit", {
+    let myForm = document.forms[0]
+    let formData = new FormData(myForm);
+    fetch("/", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: json,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
     })
-      .then(async (response) => {
-        let json = await response.json();
-        if (!response.status == 200) {
-          console.log(response);
-        }
-      })
-      .catch((error) => {
-        alert(error);
-      })
-      .then(function () {
-        form.reset();
-      });
-  });
-})();
+      .then(() => location.assign('/'))
+      .catch((error) => alert(error));
+  };
+})()
