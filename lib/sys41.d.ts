b3fc6e410879eaaf60f8d1af64e9946791d6e213
its-pablo <78528552+its-pablo@users.noreply.github.com>
2022-07-03 16:43:25 -0700

    push updated .d.ts file

--- lib/sys41.d.ts
+++ lib/sys41.d.ts
@@ -1,10 +1,32 @@
-const sys41 = {
+const sys41 : {
     user: {
-        apps: null,
+        apps: {
+            short_name: string,
+            name: string,
+            action: (text:string, title:string) => void,
+            categories: string[],
+            system: boolean,
+            permissions: string[],
+            removeable: boolean,
+        }[],
     },
-    fs: {},
-    dom: {},
-    system: {},
+    fs: {
+        async get: (key) => string | Error
+        async set: (key, value) => string | Error
+        async delete: (key) => void | Error
+        utils: {
+            async config: () => string
+            defaultDrive: RegExp
+        }
+    },
+    dom: {
+        boot: HTMLDivElement,
+        desktop: HTMLDivElement,
+        taskbar: HTMLDivElement
+    },
+    system: {
+        boot: {}
+    },
     settings: {},
     ui: {},
     Window: null
