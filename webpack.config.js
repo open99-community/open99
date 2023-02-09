be89963d60969019379516c4ea2d298f34942800
its-pablo <itspablo@null.net>
2023-02-08 17:42:35 -0800

    yes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 import path from "path"
+import { webpack } from "webpack"
 
 export default {
-    
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "public/system/scripts"),
+        filename: "sys41.min.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                use: "raw-loader"
+            }
+        ]
+    }
 }
