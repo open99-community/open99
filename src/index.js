8427e28753cf50dde607adc45b3fc95bdaa14f26
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-02-14 22:54:51 +0000

    service worker is working!! clean some stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import fsApi from "./fs/fs.js";
+import fsTask from "./boot/tasks/fs.js";
 
 window.sys41 = {
     fsApi
 }
+
+await fsTask()
--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,8 @@ export default {
                 use: "raw-loader"
             }
         ]
+    },
+    experiments: {
+        topLevelAwait: true
     }
 }
