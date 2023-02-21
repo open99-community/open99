9de895350c9d6822da44a71a658fe185dd6693f3
its-pablo <itspablo@null.net>
2023-02-20 21:04:54 -0800

    Fix idb error (thanks utf)

--- src/boot/idb.js
+++ src/boot/idb.js
@@ -1,15 +1,17 @@
-const fsTask = async () => {
+const fsTask = () => {
 
     let openRequest = window.indexedDB.open("open99", 1)
     openRequest.onerror = e => {
         console.log("Error when attempting to open database: " + e)
     }
-    let DATABASE
-    openRequest.onsuccess = e => {
-        DATABASE = openRequest.result || e.target.result
-        //and then we need to return this fsTask function 
-        //which is two levels deep and i have no clue how to do
-    }
+    return new Promise((resolve, reject) => {
+        openRequest.onsuccess = e => {
+            resolve(openRequest.result || e.target.result)
+        }
+        openRequest.onerror = e => {
+            reject("Error opening db:" + e)
+        }
+    })
 }
 
 export default fsTask
