9bbe35d78393fc2498c03c924a37a985aa45da83
its-pablo <itspablo@null.net>
2023-02-21 21:03:52 -0800

    Add stuff to bootentry

--- src/boot/idb.js
+++ src/boot/idb.js
@@ -1,17 +1,14 @@
-const fsTask = () => {
+const idb = () => {
 
     let openRequest = window.indexedDB.open("open99", 1)
-    openRequest.onerror = e => {
-        console.log("Error when attempting to open database: " + e)
-    }
     return new Promise((resolve, reject) => {
         openRequest.onsuccess = e => {
             resolve(openRequest.result || e.target.result)
         }
         openRequest.onerror = e => {
-            reject("Error opening db:" + e)
+            reject("Error attempting to open db:" + e)
         }
     })
 }
 
-export default fsTask
+export default idb
