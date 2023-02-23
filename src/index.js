bedcb07e4e9ecf2b7a0582ea62bf0038d80c5e60
its-pablo <itspablo@null.net>
2023-02-22 19:54:11 -0800

    Fix double manifest, add features to bootentry, kernel now writes to bootscreen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fsApi from "./fs/fs.js"
 import idb from "./boot/idb.js"
 import Bootscreen from "./boot/boot.js"
 
-window.sys41 = {
+const sys41 = {
     fs: fsApi,
     /**
      * the raw database
@@ -11,3 +11,11 @@ window.sys41 = {
     _db: await idb(),
     _boot: new Bootscreen(document.getElementsByClassName("boot")[0])
 }
+sys41._boot.write({text: "open99 BETA rewrite booting on " + navigator.userAgent + " at " + new Date()})
+sys41._boot.write({text: "Loading IDB..."})
+try {
+    sys41._db = await idb()
+} catch (e) {
+    sys41._boot.write({text: "IDB failed to load", error: true, blink: true})
+}
+sys41._boot.write({text: "IDB loaded!", success: true})
