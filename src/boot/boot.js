fba0cc5ce915d0dd911e29ae7047d7222c09914c
its-pablo <itspablo@null.net>
2023-02-22 19:54:11 -0800

    Fix double manifest, add features to bootentry, kernel now writes to bootscreen

--- src/boot/boot.js
+++ src/boot/boot.js
@@ -1,32 +1,53 @@
 class BootEntry {
-    constructor(content, bootscreen){
+    constructor(content, bootscreen) {
         this.content = content
         let el = document.createElement("p")
-        if(content.features.color){
-            el.style.color = content.features.color
+        const ft = content.features
+        //hacky but I don't want a bunch of if statements
+        if (ft) {
+            switch (true) {
+                case ft.color:
+                    el.style.color = ft.color
+                    break
+                case ft.error:
+                    el.classList.add("error")
+                    break
+                case ft.success:
+                    el.classList.add("success")
+                    break
+                case ft.warning:
+                    el.classList.add("warning")
+                    break
+                case ft.blink:
+                    el.classList.add("blink")
+            }
         }
+
         el.innerHTML = content.text
         bootscreen.appendChild(el)
         this.el = el
     }
-    update(content){
-        try{
+    update(content) {
+        try {
             this.el.innerHTML = content.text
-        } catch(e) {
+        } catch (e) {
             throw new Error("Could not change boot entry content")
         }
         return this
     }
-    remove(){
+    remove() {
         this.el.remove()
     }
 }
 
 export default class Bootscreen {
-    constructor(el){
+    constructor(el) {
         this.el = el
     }
-    async write(content){
+    async write(content) {
         return new BootEntry(content, this.el)
+    }
+    async error(text) {
+        return new BootEntry({ text: text })
     }
 }
