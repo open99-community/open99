44b841d32f690268d8d1ee9db6100844e88d3757
its-pablo <itspablo@null.net>
2023-06-15 15:33:48 -0700

    fix small bugs, update gitignore

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ const sys41 = {
 }
 window.sys41 = sys41
 
-sys41._boot.write({ text: "open99 BETA rewrite booting on " + navigator.userAgent + " at " + new Date() })
+await sys41._boot.write({ text: "open99 BETA rewrite booting on " + navigator.userAgent + " at " + new Date() })
 
 //IDB
-sys41._boot.write({ text: "Loading IDB..." })
-const idb = await import("./boot/idb.js")
+await sys41._boot.write({ text: "Loading IDB..." })
+const idb = await import("./fs/idb.js")
 try {
-    sys41._db = await idb()
+    sys41._db = await idb.default
 } catch (e) {
-    sys41._boot.write({ text: "IDB failed to load", features: { error: true, blink: true } })
+    await sys41._boot.write({ text: "IDB failed to load", features: { error: true, blink: true } })
 }
-sys41._boot.write({ text: "IDB loaded!", features: { success: true } })
+await sys41._boot.write({ text: "IDB loaded!", features: { success: true } })
 
 //FS
-sys41._boot.write({ text: "Loading FileSystem..." })
+await sys41._boot.write({ text: "Loading FileSystem..." })
 const fsApi = await import("./fs/fs.js")
 sys41.fs = fsApi
-sys41._boot.write({ text: "Filesystem loaded!", features: { success: true } })
+await sys41._boot.write({ text: "Filesystem loaded!", features: { success: true } })
 
 //41WORKER
-sys41._boot.write({text: "Loading application runtime..."})
+await sys41._boot.write({text: "Loading application runtime..."})
 const AppRuntime = await import("./41worker/AppRuntime.js")
 sys41.AppRuntime = AppRuntime
-sys41._boot.write({text: "Application runtime loaded!", features: {success: true}})
+await sys41._boot.write({text: "Application runtime loaded!", features: {success: true}})
