6a0c14d6b703d85ede3a94e8b71061a4d8d76b10
its-pablo <itspablo@null.net>
2023-06-27 14:07:17 -0700

    fix idb.js problems

--- src/fs/idb.js
+++ src/fs/idb.js
@@ -1,22 +1,19 @@
 /**
  * IDB
- * @returns {Promise<IDBDatabase> | Promise<Error>}
+ * @returns {IDBDatabase}
  */
-const idb = () => {
+const idb = async () => {
     let openRequest = window.indexedDB.open("open99", 1)
     return new Promise((resolve, reject) => {
-        openRequest.onsuccess = e => {
-            resolve(openRequest.result || e.target.result)
+        openRequest.onsuccess = () => {
+            resolve(openRequest.result)
         }
         openRequest.onerror = e => {
             reject("Error attempting to open db:" + e)
         }
     })
 }
-/**
- * lets just assume this isn't an error for now
- * @type {IDBDatabase}
- */
+
 const result = await idb()
 result.onversionchange = (ev) => {
     console.log("IndexedDB version change detected", ev)
