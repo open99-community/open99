cb3f870cc4eff71b8a8a12d6fe0342d7b40f02c3
its-pablo <itspablo@null.net>
2023-07-12 20:21:49 -0700

    remove boot garbage

--- src/index.js
+++ src/index.js
@@ -3,37 +3,18 @@ import idb from "./fs/idb.js"
 import {loadRootFs} from "./fs/loadRootFs.js"
 import fsApi from "./fs/fs.js"
 import AppRuntime from "./41worker/AppRuntime.js"
-
-
-const sys41 = {
-    _boot: new Bootscreen(document.getElementsByClassName("boot")[0])
-}
+const sys41 = {}
 if (process.env.NODE_ENV === "development") {
     window.sys41 = sys41
 }
+sys41._boot = new Bootscreen(document.getElementsByClassName("boot")[0])
+sys41._db = idb
+sys41.fs = fsApi
+sys41.AppRuntime = AppRuntime
 
 sys41._boot.write({ text: "open99 BETA rewrite booting on " + navigator.userAgent + " at " + new Date() })
 
-//IDB
-sys41._boot.write({ text: "Loading IDB..." })
-try {
-    sys41._db = idb
-} catch (e) {
-    sys41._boot.write({ text: "IDB failed to load", features: { error: true, blink: true } })
-}
-sys41._boot.write({ text: "IDB loaded!", features: { success: true } })
-
 //ROOTFS-LOAD
 sys41._boot.write({text: "Writing root fs..."})
-loadRootFs()
+await loadRootFs()
 sys41._boot.write({text: "Root filesystem loaded!", features: {success: true}})
-
-//FS
-sys41._boot.write({ text: "Loading FileSystem..." })
-sys41.fs = fsApi
-sys41._boot.write({ text: "Filesystem loaded!", features: { success: true } })
-
-//41WORKER
-sys41._boot.write({text: "Loading application runtime..."})
-sys41.AppRuntime = AppRuntime
-sys41._boot.write({text: "Application runtime loaded!", features: {success: true}})
