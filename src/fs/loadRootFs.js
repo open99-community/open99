78365dd3241eec41a8a5c26c5f364d157b48b491
its-pablo <itspablo@null.net>
2023-07-19 11:02:18 -0700

    blob, not base64

--- src/fs/loadRootFs.js
+++ src/fs/loadRootFs.js
@@ -7,7 +7,7 @@ import fileSystemItem from "./FileSystemItem.ts"
 export async function load() {
     let zip = await JSZip.loadAsync(await (await fetch("/assets/rootfs.zip")).blob())
     await zip.forEach(async (relativePath, file) => {
-        const content = await file.async("base64")
+        const content = await file.async("blob")
         const item = new fileSystemItem({
             path: relativePath,
             content: content
