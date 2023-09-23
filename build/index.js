7c6659e8c8224cda2279628466a8669fb2c178bd
its-pablo <itspablo@null.net>
2023-09-23 01:14:14 -0700

    fix tauri support

--- build/index.js
+++ build/index.js
@@ -2,6 +2,8 @@ import tasks from "./tasks/index.js"
 import { LogSession } from "portalog"
 import { config } from "dotenv"
 config()
+process.env.NODE_ENV = process.argv.includes("--devmode") ? "development" : process.env.NODE_ENV || "production"
+process.env.NODE_ENV = process.argv.includes("--prodmode") ? "production" : process.env.NODE_ENV || "production"
 
 const session = new LogSession
 const logger = session.addItem(`PLUTO OS building on ${new Date()}\nin ${process.env.NODE_ENV} mode. Watch mode is ${process.argv.includes("--watch") ? "on" : "off"}`, "🌌")
