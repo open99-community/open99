43a2ddc046fd17ecee5aae619b0fa7867c5c75ad
443pablo <172942547+443pablo@users.noreply.github.com>
2024-11-14 23:04:36 -0800

    feat: remove useless test code from before

--- installer_fs/Pluto/System41/start/index.js
+++ installer_fs/Pluto/System41/start/index.js
@@ -9,74 +9,9 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const e = eval
-
-const originalSetTimeout = self.setTimeout;
-const originalSetInterval = self.setInterval;
-let heartbeatInterval = null;
-let lastHeartbeatTime = null;
-
-function startHeartbeat() {
-    const HEARTBEAT_INTERVAL = 5000; // 5 seconds
-    const HEARTBEAT_CHECK_INTERVAL = 1000; // 1 second
-
-    // Send initial heartbeat
-    sendHeartbeat();
-
-    // Set up interval to check if heartbeat should be sent
-    this.heartbeatInterval = self.setInterval(() => {
-        const currentTime = Date.now();
-        if (currentTime - lastHeartbeatTime >= HEARTBEAT_INTERVAL) {
-            sendHeartbeat();
-        }
-    }, HEARTBEAT_CHECK_INTERVAL);
-}
-
-function sendHeartbeat() {
-    lastHeartbeatTime = Date.now();
-    comms.sendStreamEvent(2);
-}
-
-function resetHeartbeat() {
-    lastHeartbeatTime = Date.now();
-}
-
-
-self.setTimeout = function(handler, timeout, ...args) {
-    const wrappedHandler = function(...handlerArgs) {
-        resetHeartbeat();
-        if (typeof handler === 'function') {
-            handler.apply(this, handlerArgs);
-        } else {
-            e(handler);
-        }
-    };
-    return originalSetTimeout.call(this, wrappedHandler, timeout, ...args);
-};
-
-self.setInterval = function(handler, timeout, ...args) {
-    const wrappedHandler = function(...handlerArgs) {
-        resetHeartbeat();
-        if (typeof handler === 'function') {
-            handler.apply(this, handlerArgs);
-        } else {
-            e(handler);
-        }
-    };
-    return originalSetInterval.call(this, wrappedHandler, timeout, ...args);
-};
-
-
-console.log("sleeping for 5 seconds")
-await sleep(5000)
-console.log("done sleeping")
-
+console.log("test program")
 const comms = Pluto._core.CommsHandler.getInstance();
 
-await comms.call("fs.createFile");
+const body = await comms.call(70);
 
-startHeartbeat();
-console.log("test")
-const int = setInterval(() => {console.log("duhh")}, 1000);
-setTimeout(() => {clearInterval(int)}, 5000)
-//while (true) {}
+console.log(body);
