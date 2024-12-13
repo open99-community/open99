0bf48672366feb9381f8d06222c37facd9c7543b
stretch07 <78528552+stretch07@users.noreply.github.com>
2024-12-12 16:01:25 -0800

    fix: remove extra debug info

--- scripts/postbuild/index.js
+++ scripts/postbuild/index.js
@@ -36,7 +36,7 @@ if (installerBytes === 0 || rootBytes === 0) {
 }
 
 if (process.argv.includes("--watch")) {
-  console.log("Starting server...", "info");
+  console.log("Starting server...");
   (async () => {
     http
       .createServer((req, res) => {
@@ -47,7 +47,7 @@ if (process.argv.includes("--watch")) {
       })
       .listen(process.env.DEV_SERVER_PORT ?? 8000, async () => {
         const url = `http://localhost:${process.env.DEV_SERVER_PORT ?? 8000}`;
-        console.log(`Server running at ${url}`, "success");
+        console.log(`Server running at ${url}`);
       });
   })();
 }
